2c21bd6057ab72115e88dfcc06cc92b496bb1c01
Ryan <real.rezz@gmail.com>
2017-01-25 18:00:43 +0700

    added Forecast

---
+++ WeatherApp/CustomViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomViewCell.swift
+//  WeatherApp
+//
+//  Created by iMac on 1/25/17.
+//  Copyright © 2017 Ari Fajrianda Alfi. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
